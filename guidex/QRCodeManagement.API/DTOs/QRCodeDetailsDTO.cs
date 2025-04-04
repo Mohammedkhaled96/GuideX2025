df6211a46ec9cdecef30b7071805d1ed8f936738
mohamed khaled <mohamed.khaled@moneyfellows.com>
2025-04-04 04:10:39 +0200

    delete old  version

--- guidex/QRCodeManagement.API/DTOs/QRCodeDetailsDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace QRCodeManagement.API.DTOs
-{
-    public class QRCodeDetailsDTO
-    {
-        public Models.QRCode QRCode { get; set; } = null!;
-        public Models.Item Item { get; set; } = null!;
-        public Models.User User { get; set; } = null!;
-    }
-}
